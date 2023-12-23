1c138b1b19cea75ec7a92b7823466baf1d00325b
Nazmus Sakib <58693047+nsakib176@users.noreply.github.com>
2023-12-23 17:29:07 +0600

    Database and models

---
+++ database/migrations/2023_12_23_111544_create_recipes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->text('ingredients'); // Assuming a JSON string for ingredients
+            $table->text('instructions');
+            $table->text('dietary_restrictions'); // Assuming a comma-separated list
+            $table->string('image')->nullable(); // For storing image paths
+            $table->foreignId('user_id')->constrained(); // Foreign key to users table
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
