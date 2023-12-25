e557033112c40a916123513732dc4e66fa013949
Nazmus Sakib <58693047+nsakib176@users.noreply.github.com>
2023-12-25 08:11:17 +0600

    Update and Delete Method Implemented

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -6,6 +6,7 @@ use App\Models\Recipe;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class RecipeController extends Controller
 {
@@ -37,7 +38,7 @@ class RecipeController extends Controller
     }
 
     // Show a specific recipe
-    public function show(Request $request, $id)
+    public function show($id)
     {
         try {
             $recipe = Recipe::findOrFail($id);
@@ -95,6 +96,94 @@ class RecipeController extends Controller
         } catch (\Exception $e) {
             return response()->json([
                 'message' => 'Error creating recipe',
+                'error' => $e->getMessage()
+            ], 500);  // Return 500 Internal Server Error status code
+        }
+    }
+
+    // Update an existing recipe
+    public function update(Request $request, $id)
+    {
+        try {
+            $recipe = Recipe::findOrFail($id);
+
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'required|string',
+                'ingredients' => 'required|string',
+                'instructions' => 'required|string',
+                'dietary_restrictions' => 'required|string',
+                'image' => 'nullable|image|mimes:jpg,jpeg,png|max:2048', // Optional image validation
+            ]);
+
+            // check if the user is authorize to update the recipe
+            if ($recipe->user_id != Auth::user()->id) {
+                return response()->json([
+                    'message' => 'Unauthorized to update this recipe'
+                ], 401);  // Return 401 Unauthorized status code
+            }
+
+            $recipe->fill($request->only([
+                // Fields to fill from request data
+                'name',
+                'description',
+                'ingredients',
+                'instructions',
+                'dietary_restrictions',
+                'image',
+            ]));
+
+            // Handle image upload if provided
+            if ($request->hasFile('image')) {
+                //delete the old file
+                if ($recipe->image != null) {
+                    Storage::delete($recipe->image);  // Adjust path as needed
+                    $recipe->image = null;  // Set the image path in the recipe model
+                }
+                //save new image
+                $imagePath = $request->file('image')->store('public/recipe_images');  // Adjust path as needed
+                $recipe->image = $imagePath;  // Set the image path in the recipe model
+            }
+            $recipe->save();
+            return response()->json([
+                'message' => 'Recipe updated successfully',
+                'Recipe' => $recipe
+            ], 201);  // Return 201 Created status code
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Error updating recipe',
+                'error' => $e->getMessage()
+            ], 500);  // Return 500 Internal Server Error status code
+
+        }
+    }
+
+    // Delete a recipe
+    public function destroy(Request $request, $id)
+    {
+        try {
+            $recipe = Recipe::findOrFail($id);
+
+            // check if the user is authorize to delete the recipe
+            if ($recipe->user_id != Auth::user()->id) {
+                return response()->json([
+                    'message' => 'Unauthorized to delete this recipe'
+                ], 401);  // Return 401 Unauthorized status code
+            }
+
+            // Delete image if present
+            if ($recipe->image) {
+                Storage::delete($recipe->image);  // Delete image file
+            }
+
+            $recipe->delete();
+
+            return response()->json([
+                'message' => 'Recipe deleted successfully'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Error updating recipe',
                 'error' => $e->getMessage()
             ], 500);  // Return 500 Internal Server Error status code
         }
